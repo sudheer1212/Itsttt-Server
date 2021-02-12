f41db600923af6fbcaac6ad414615b54006c5a42
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-02-12 16:28:29 +0530

    minor change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const app = express(); 
-// app.use(cors());
+app.use(cors());
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "https://localhost:3000");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
